603c1a94bd01ba09c4fdd1124706ed174712c0f5
sawanoboly <sawanoboriyu@higanworks.com>
2013-06-16 17:39:49 +0900

    remove debbug line

--- send_irv.rb
+++ send_irv.rb
@@ -6,7 +6,6 @@ require 'yaml'
 @irc = YAML.load_file('./config/irc.yml')
 
 $stdin.each_line do |line|
-  puts "irc://#{@irc['nick']}:#{@irc['pass']}@#{@irc['server']}:/##{@irc['channel']}"
   CarrierPigeon.send(
     :uri => "irc://#{@irc['nick']}:#{@irc['pass']}@#{@irc['server']}:#{@irc['port']}/##{@irc['channel']}",
     :message => line,
